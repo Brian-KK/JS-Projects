49eb5b88c848f54aca390e181fb1e07533917d59
Brian Koekkoek <bjkoekkoek@hotmail.com>
2022-04-03 13:13:30 +0200

    Design page

--- Project-1/project-1.js
+++ Project-1/project-1.js
@@ -1,5 +1,6 @@
-const btn_main = document.querySelector('.btn-main');
+const days = document.querySelector('.days').innerText;
+const hours = document.querySelector('.hours').innerText;
+const mins = document.querySelector('.mins').innerText;
+const seconds = document.querySelector('.seconds').innerText;
 
-btn_main.addEventListener('click', function () {
-	window.location = '../index.html';
-});
+console.log(days, hours, mins, seconds);

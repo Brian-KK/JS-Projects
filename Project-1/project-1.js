3ce117f0add53a9e306b2b44f54149f2e818d348
Brian Koekkoek <bjkoekkoek@hotmail.com>
2022-04-03 13:24:42 +0200

    Add project-1 thumbnail

--- Project-1/project-1.js
+++ Project-1/project-1.js
@@ -1,6 +1,12 @@
+const btn_main = document.querySelector('.btn-main');
+
 const days = document.querySelector('.days').innerText;
 const hours = document.querySelector('.hours').innerText;
 const mins = document.querySelector('.mins').innerText;
 const seconds = document.querySelector('.seconds').innerText;
 
 console.log(days, hours, mins, seconds);
+
+btn_main.addEventListener('click', function () {
+	window.location = '../index.html';
+});
